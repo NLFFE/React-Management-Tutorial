ef8f434adb98729fcb537cf9bcb14937ac50126f
nlffe <acc1797@naver.com>
2020-06-20 19:15:54 +0900

    Apply Material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 
 
 const customers = [
@@ -30,14 +37,26 @@ const customers = [
   },
 ]
 
+
 function App() {
   return (
-    <div>
-    {
-      customers.map(c =>{return( <Customer key={c.id} id={c.id} image={c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>)})
-    }
-    
-    </div>
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {customers.map(c =>{return( <Customer key={c.id} id={c.id} image={c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>)})}
+      </TableBody>
+      </Table>
+    </Paper>
   );
 }
 

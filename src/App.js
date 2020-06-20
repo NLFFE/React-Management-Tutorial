7705ade2082203a4b7887888d50d0112ca4725af
nlffe <acc1797@naver.com>
2020-06-20 18:55:47 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+
+const customers = [
+  {
+    'id':'1',
+    'image':'https://placeimg.com/64/64/1',
+    'name': '김진뉴1',
+    'birthday':'961111',
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':'2',
+    'image':'https://placeimg.com/64/64/2',
+    'name': '김진뉴2',
+    'birthday':'961111',
+    'gender':'중성',
+    'job':'프로그래머'
+  },
+  {
+    'id':'3',
+    'image':'https://placeimg.com/64/64/any3',
+    'name': '김진뉴3',
+    'birthday':'12345',
+    'gender':'여자',
+    'job':'디자이너'
+  },
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo"/>
-      <h1>Let's develop</h1>
+    <div>
+    {
+      customers.map(c =>{return( <Customer key={c.id} id={c.id} image={c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job}/>)})
+    }
+    
     </div>
   );
 }
